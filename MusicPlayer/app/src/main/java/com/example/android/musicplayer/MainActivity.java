72eac4457f2e5da5fcb3e755b1855dec846a761a
yousef-standard <yousefhabood@gmail.com>
2020-12-10 10:26:42 +0100

    Fully functioning Music Player application with 11 Classical Music tracks, Added HomePage, Scrolling TextView in the case of insufficient screen space, 11 Separate activities that can be navigated through using 2 buttons (Next & Previous) & a Resume/Pause button, an icon that can take you to the homepage!

--- MusicPlayer/app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -2,20 +2,122 @@ package com.example.android.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView song1, song2, song3, song4, song5, song6, song7, song8, song9, song10, song11;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        song1 = this.findViewById(R.id.song1);
+        song1.setSelected(true);
+
+        song2 = this.findViewById(R.id.song2);
+        song2.setSelected(true);
+
+        song3 = this.findViewById(R.id.song3);
+        song3.setSelected(true);
+
+        song4 = this.findViewById(R.id.song4);
+        song4.setSelected(true);
+
+        song5 = this.findViewById(R.id.song5);
+        song5.setSelected(true);
+
+        song6 = this.findViewById(R.id.song6);
+        song6.setSelected(true);
+
+        song7 = this.findViewById(R.id.song7);
+        song7.setSelected(true);
+
+        song8 = this.findViewById(R.id.song8);
+        song8.setSelected(true);
+
+        song9 = this.findViewById(R.id.song9);
+        song9.setSelected(true);
+
+        song10 = this.findViewById(R.id.song10);
+        song10.setSelected(true);
+
+        song11 = this.findViewById(R.id.song11);
+        song11.setSelected(true);
+
     }
 
-    public void goNextPage(View view){
-        Intent i = new Intent(this, songTwo.class);
+    public void firstTrack(View view){
+        Intent i = new Intent(this, song1.class);
         startActivity(i);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
     }
+
+    public void secondTrack(View view){
+        Intent i = new Intent(this, song2.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+    public void thirdTrack(View view){
+        Intent i = new Intent(this, song3.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+    public void fourthTrack(View view){
+        Intent i = new Intent(this, song4.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+    public void fifthTrack(View view){
+        Intent i = new Intent(this, song5.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+    public void sixthTrack(View view){
+        Intent i = new Intent(this, song6.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+    public void seventhTrack(View view){
+        Intent i = new Intent(this, song7.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+    public void eighthTrack(View view){
+        Intent i = new Intent(this, song8.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+    public void ninthTrack(View view){
+        Intent i = new Intent(this, song9.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+    public void tenthTrack(View view){
+        Intent i = new Intent(this, song10.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+    public void eleventhTrack(View view){
+        Intent i = new Intent(this, song11.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
 }
---
+++ MusicPlayer/app/src/main/java/com/example/android/musicplayer/song9.java
@@ -0,0 +1,73 @@
+package com.example.android.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class song9 extends AppCompatActivity {
+    MediaPlayer player;
+    TextView track1, author;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song9);
+        ImageButton playButton = findViewById(R.id.imageButton4);
+        player = MediaPlayer.create(this, R.raw.track10);
+        track1 = (TextView) this.findViewById(R.id.holstOp32);
+        track1.setSelected(true);
+
+        author = (TextView) this.findViewById(R.id.author);
+        author.setSelected(true);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!(player.isPlaying())) {
+                    player.start();
+                    playButton.setImageResource(R.drawable.pause);
+                } else {
+                    player.pause();
+                    playButton.setImageResource(R.drawable.play);
+                }
+            }
+        });
+
+        if(!(player.isPlaying())){
+            player.start();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        player.pause();
+    }
+
+
+
+    public void goHomePage(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    public void previousSong(View view) {
+        Intent i = new Intent(this, song8.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    public void nextSong(View view) {
+        Intent i = new Intent(this, song10.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+
+}
